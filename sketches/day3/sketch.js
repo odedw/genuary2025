239c58c12de49f049b00ed38494682cff0c95fd2
Oded Welgreen <oded.welgreen@gmail.com>
2024-12-23 22:28:22 -0600

    progress on day 3

--- sketches/day3/sketch.js
+++ sketches/day3/sketch.js
@@ -1,81 +1,82 @@
 /// <reference types="p5/global" />
-const config = { width: 700, height: 700, record: { shouldRecord: false, duration: 120 }, cellSize: 50, particleSize: 10 };
+const config = { width: 700, height: 700, record: { shouldRecord: false, duration: 120 }, cellSize: 70, particleSize: 20, numberOfParticles: 10 };
 
+// Create engine with improved simulation parameters
+let engine = Matter.Engine.create({ positionIterations: 8, velocityIterations: 8 });
+randomPalette();
 class Particle {
   constructor(x, y) {
-    this.size = config.particleSize;
-    this.vel = createVector(random(-1, 1), random(-1, 1));
-    this.pos = createVector(x, y);
+    this.body = Matter.Bodies.circle(x, y, config.particleSize / 2, {
+      restitution: 0.8,
+    });
+    this.color = random(PALETTE);
+    Matter.Composite.add(engine.world, this.body);
   }
 
   draw() {
-    ellipse(this.pos.x, this.pos.y, this.size, this.size);
-  }
-
-  update() {
-    const cell = cells.find((cell) => dist(this.pos.x, this.pos.y, cell.x, cell.y) < config.cellSize);
-    // bounce off the cell line
-    if (cell && cell.orientation === 1) {
-    }
-
-    this.pos.add(this.vel);
+    fill(this.color);
+    noStroke();
+    ellipse(this.body.position.x, this.body.position.y, config.particleSize, config.particleSize);
   }
 }
 
 class Cell {
   constructor(x, y) {
-    this.x = x;
-    this.y = y;
     this.orientation = random() > 0.5 ? 1 : -1;
+    this.a = (PI / 4) * this.orientation;
+    this.body = Matter.Bodies.rectangle(x + config.cellSize / 2, y + config.cellSize / 2, 5, sqrt(2 * config.cellSize * config.cellSize), { angle: this.a, isStatic: true, friction: 0.1 });
+    this.h = sqrt(2 * config.cellSize * config.cellSize);
+    Matter.Composite.add(engine.world, this.body);
+    this.timer = Timing.frames(120, { loop: false, autoTrigger: false });
   }
 
   draw() {
-    if (this.orientation === 1) {
-      line(this.x, this.y, this.x + config.cellSize, this.y + config.cellSize);
-    } else {
-      line(this.x + config.cellSize, this.y, this.x, this.y + config.cellSize);
+    this.body.angle = this.a - this.orientation * PI * this.timer.elapsed;
+    if (this.timer.finished) {
+      this.timer.reset();
+      this.orientation = -this.orientation;
+      this.a = this.body.angle;
     }
+    if (random() < 0.0005 && !this.timer.active) {
+      this.timer.activate();
+    }
+    const pos = this.body.position;
+    const angle = this.body.angle;
+    push();
+    fill(255);
+    translate(pos.x, pos.y);
+    rotate(angle);
+    rect(0, 0, 1, this.h);
+    pop();
   }
-
-  update() {}
 }
 
-const cells = [];
-const particles = [];
+const objects = [];
 
 function setup() {
   createCanvas(config.width, config.height);
-  stroke(255);
+  fill(255);
+  noStroke();
   rectMode(CENTER);
-  strokeWeight(2);
-  noFill();
 
   for (let x = 0; x < width; x += config.cellSize) {
     for (let y = 0; y < height; y += config.cellSize) {
-      cells.push(new Cell(x, y, config.cellSize));
+      objects.push(new Cell(x, y, config.cellSize));
     }
   }
-
-  particles.push(new Particle(width / 2, height / 2));
+  for (let i = 0; i < config.numberOfParticles; i++) {
+    objects.push(new Particle(random(width), random(height)));
+  }
 }
 
 function draw() {
+  // background(0, 0, 0, 20);
   background(0);
-  cells.forEach((cell) => {
-    cell.update();
-    cell.draw();
-  });
-  particles.forEach((particle) => {
-    particle.update();
-    particle.draw();
+  Matter.Engine.update(engine);
+  objects.forEach((object) => {
+    object.draw();
   });
 }
-
-// let isLooping = true;
-// function mouseClicked() {
-//   isLooping = !isLooping;
-//   isLooping ? loop() : noLoop();
-// }
 
 if (config.record.shouldRecord) {
   P5Capture.setDefaultOptions({
